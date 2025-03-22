695704e33001057828f0dfd12bb8815512ddf258
GeonHo Kim <svvvs5579@naver.com>
2025-03-22 10:40:32 +0900

    feat: 네트워크 체크 커스텀 훅 생성

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,15 +1,23 @@
 import {useLocation} from "react-router-dom";
 import {Navbar} from "./content/Navbar";
 import {MAIN} from "../../routes/Route";
+import {useNetWorkCheck} from "../../hooks/useNetworkCheck";
 
 export const Sidebar = () => {
     const locationPath = useLocation().pathname;
+    const { isOnline } = useNetWorkCheck();
 
     return (
         <aside className={`${locationPath !== MAIN ? "h-[calc(100vh-50px)] top-[50px]" : "h-[calc(100vh-100px)] top-[100px]"} fixed left-0 w-[350px]  bg-white z-[10] overflow-hidden border-r border-solid border-[#DEDEDE]`}>
-            <ul className="flex flex-col gap-2 p-2 h-full w-full">
+            <ul className="flex flex-col gap-2 p-2 h-[calc(100vh-160px)] overflow-y-hidden w-full">
                 <Navbar  />
             </ul>
+
+            <div className="w-full border-t border-solid border-[#DEDEDE] h-[56px] absolute bottom-0 left-0 flex items-center">
+                <div className={`${isOnline ? "" : "bg-[#DEDEDE] bg-opacity-70"} w-[52px] h-full border-r border-solid border-[#DEDEDE] flex items-center justify-center`}>
+                    <img src="/images/icon/16x16/ico_signal.png" />
+                </div>
+            </div>
         </aside>    
     )
 };
---
+++ src/hooks/useNetworkCheck.tsx
@@ -0,0 +1,21 @@
+import {useEffect, useState} from "react";
+
+export const useNetWorkCheck = () => {
+    const [isOnline, setIsOnline] = useState(navigator.onLine);
+
+    const handleNetworkChange = () => {
+            setIsOnline(navigator.onLine);
+        };
+    
+    useEffect(() => {
+        window.addEventListener('online', handleNetworkChange);
+        window.addEventListener('offline', handleNetworkChange);
+
+        return () => {
+            window.removeEventListener('online', handleNetworkChange);
+            window.removeEventListener('offline', handleNetworkChange);
+        };
+    }, []);
+
+    return { isOnline };
+};
